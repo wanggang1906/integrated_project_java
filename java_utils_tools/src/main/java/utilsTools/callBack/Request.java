0cbe7c7480b8f85b0f27d40d22b4433cb22891f6
wanggang1906 <1294901911@qq.com>
2020-04-24 18:08:41 +0800

    java回调方法

--- java_utils_tools/src/main/java/utilsTools/callBack/Request.java
+++ java_utils_tools/src/main/java/utilsTools/callBack/Request.java
@@ -21,6 +21,14 @@ public class Request {
 
 }
 
+/**
+ * 1、先定义一个回调接口CallBack，定义好回调方法onResonse(String data)；
+ * 2、Response类中的handle方法有两个参数，一个字符串，一个接收Request的回调方法
+ * 3、Request类新启动一个线程去调用Resonpse的handle方法
+ * 第二个参数就是callBack的实现，也就是真正的回调方法
+ * 4、最后，callBack.onResponse("请求完成，响应success") 这一句就是在调用回调方法了
+ * */
+
 class Response {
 
     public void handle(String msg, CallBack callBack) {
